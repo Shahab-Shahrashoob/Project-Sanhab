79c8380c6d79a723c847cd32fc964f06db3c7f43
sania-dolat <120238420+sania-dolat@users.noreply.github.com>
2022-12-10 10:34:56 +0330

    11

--- battle ship phase 1.c
+++ battle ship phase 1.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
 
-int main()
+int board(const int n)
 {
-    int n,i,j;
-    scanf("%d",&n);
-    char player1[n+1][n+1];
-    char player2[n+1][n+1];
+    int i,j;
+    char FOCP1[n+1][n+1];
+    char FOCP2[n+1][n+1];
     //---------------------------------------------------------------------------------//
     for(i=0;i<n+1;i++)                                       /*Grid for player 1 board*/
     {
-        player1[0][i]=i+48;                                      
-        player1[i][0]=i+48;
-    }
-    for(i=0;i<n+1;i++)
-    {
-        player1[0][i]=i+48;                                 /*Grid for player 1 board*/
-        player1[i][0]=i+48;
+        FOCP1[0][i]=i+48;                                      
+        FOCP1[i][0]=i+48;
     }
     //--------------------------------------------------------------------------------//
     for(i=1;i<n+1;i++)                                  /*Filling the sea of player 1*/
     {
-        for(j=1;j<n+1;j++) {player1[i][j]='~';}                 
+        for(j=1;j<n+1;j++) {FOCP1[i][j]='~';}                 
     }
     //--------------------------------------------------------------------------------//
     for(i=0;i<n+1;i++)                                      /*Grid for player 2 board*/
     {                                                       
-        player2[0][i]=i+48;                                  
-        player2[i][0]=i+48;
+        FOCP2[0][i]=i+48;                                  
+        FOCP2[i][0]=i+48;
     }
     //--------------------------------------------------------------------------------//
     for(i=1;i<n+1;i++)                                  /*Filling the sea of player 2*/ 
     {
-        for(j=1;j<n+1;j++) {player2[i][j]='~';}                
+        for(j=1;j<n+1;j++) {FOCP2[i][j]='~';}                
     }
     //--------------------------------------------------------------------------------//
     for(i=0;i<n+1;i++)                                          /*Printing the boards*/
@@ -41,18 +35,18 @@ int main()
             for(j=0;j<n+1;j++)
             {
                 if(i>0&&j>0)printf("\033\e[0;34m");
-                printf("%c ",player1[i][j]);
+                printf("%c ",FOCP1[i][j]);
                 printf("\033[0m");
             }
             printf("  |   ");                                    
             for(j=0;j<n+1;j++)
             {
                 if(i>0&&j>0)printf("\033\e[0;34m");
-                printf("%c ",player2[i][j]);
+                printf("%c ",FOCP2[i][j]);
                 printf("\033[0m");
             }
             printf("\n");
         }
     }
-    return 0;
+    return 1;
 }
