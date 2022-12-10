cf90c55afce83500f8e723ff0453369d1fdd1856
sania-dolat <120238420+sania-dolat@users.noreply.github.com>
2022-12-10 10:18:46 +0330

    1

--- battle ship phase 1.c
+++ battle ship phase 1.c
@@ -12,6 +12,11 @@ int main()
         player1[0][i]=i+48;                                      
         player1[i][0]=i+48;
     }
+    for(i=0;i<n+1;i++)
+    {
+        player1[0][i]=i+48;                                 /*Grid for player 1 board*/
+        player1[i][0]=i+48;
+    }
     //--------------------------------------------------------------------------------//
     for(i=1;i<n+1;i++)                                  /*Filling the sea of player 1*/
     {
@@ -31,24 +36,23 @@ int main()
     //--------------------------------------------------------------------------------//
     for(i=0;i<n+1;i++)                                          /*Printing the boards*/
     {
-        for(j=0;j<n+1;j++)printf("%c ",player1[i][j]);
-        printf("  |  ");                                     
-        for(j=0;j<n+1;j++)printf("%c ",player2[i][j]);
-    for(i=0;i<n+1;i++){
-        for(j=0;j<n+1;j++){
-            if(i>0&&j>0)printf("\033\e[0;34m");
-            printf("%c ",player1[i][j]);
-            printf("\033[0m");
+        for(i=0;i<n+1;i++)
+        {
+            for(j=0;j<n+1;j++)
+            {
+                if(i>0&&j>0)printf("\033\e[0;34m");
+                printf("%c ",player1[i][j]);
+                printf("\033[0m");
+            }
+            printf("  |   ");                                    
+            for(j=0;j<n+1;j++)
+            {
+                if(i>0&&j>0)printf("\033\e[0;34m");
+                printf("%c ",player2[i][j]);
+                printf("\033[0m");
+            }
+            printf("\n");
         }
-        printf("  |   ");                                     /*Printing the boards*/
-        for(j=0;j<n+1;j++){
-            if(i>0&&j>0)printf("\033\e[0;34m");
-            printf("%c ",player2[i][j]);
-            printf("\033[0m");
-        }
-
-        printf("\n");
     }
     return 0;
-    }
 }
