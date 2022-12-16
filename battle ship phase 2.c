bdefe3bc0760a1131f91bb145dbfcd0ebe6492eb
sania-dolat <120238420+sania-dolat@users.noreply.github.com>
2022-12-16 21:42:14 +0330

    1

--- battle ship phase 2.c
+++ battle ship phase 2.c
@@ -17,6 +17,25 @@ void reset()
     printf("\033[0;m");
 }
 
+void horizonal(int x, int y, char a[][30], int m)
+{
+    int i;
+    for (i = 0; i < m; i++)
+    {
+        a[x][y+i] = '+';
+    }
+}
+
+void vertical(int x, int y, char a[][30], int m)
+{
+    int i;
+    for (i = 0; i < m; i++)
+    {
+        a[x+i][y] = '+';
+    }
+}
+
+
 int main()
 {
     int i, j, n, len1, len2, delta, ship;
@@ -30,17 +49,15 @@ int main()
     char FOCP1[n + 1][n + 1];
     char FOCP2[n + 1][n + 1];
     //---------------------------------------------------------------------------------//
-    scanf("%s",name1);                                       /*Scaning the coordinates*/
+    fgets(name1,20,stdin);                                     /*Scaning the coordinates*/
     for(i=0;i<ship;i++){
         scanf("%c %c %c",ship1[i][0],ship1[i][1],ship1[i][2]);
     }
     scanf("%s",bin);
-    scanf("%s",name2);
+    fgets(name2,20,stdin);
     for(i=0;i<ship;i++){
         scanf("%c %c %c",ship2[i][0],ship2[i][1],ship2[i][2]);
     }
-    //---------------------------------------------------------------------------------//
-    
     //---------------------------------------------------------------------------------//
     for (i = 0; i < n + 1; i++) /*Grid for player 1 board*/
     {
@@ -70,10 +87,36 @@ int main()
         }
     }
     //--------------------------------------------------------------------------------//
-    printf("\n\nFOCP1"); /*printing players' names*/
-    for (i = 0; i <= 2 * n + 3; i++)
-        printf(" ");
-    printf("FOCP2");
+    for(i=0;i<ship;i++)
+    {
+        if(ship1[i][2]=='h') horizonal(ship1[i][0],ship1[i][1],FOCP1,3);
+        else if(ship1[i][2]=='v') vertical(ship1[i][0],ship1[i][1],FOCP1,3);
+    }
+    for(i=0;i<ship;i++)
+    {
+       if(ship2[i][2]=='h') horizonal(ship2[i][0],ship2[i][1],FOCP2,3);
+        else if(ship2[i][2]=='v') vertical(ship2[i][0],ship2[i][1],FOCP2,3); 
+    }
+
+    //--------------------------------------------------------------------------------//
+    len1 = strlen(name1); /*printing players'names*/
+    len2 = strlen(name2);
+    delta = len1 - len2;
+    for (i = 0; i < len1; i++)
+        printf("%c", name1[i]);
+    if (delta >= 0)
+    {
+        for (i = 0; i < (2 * n + 4) - delta; i++)
+            printf(" ");
+    }
+    else if (delta < 0)
+    {
+        for (i = 0; i < 2 * n + 4; i++)
+            printf(" ");
+    }
+    for (i = 0; i < len2; i++)
+        printf("%c", name2[i]);
+    printf("\n");
     printf("\nremaing ships:%d", ship);
     for (i = 0; i <= 2 * n - 7; i++)
         printf(" ");
